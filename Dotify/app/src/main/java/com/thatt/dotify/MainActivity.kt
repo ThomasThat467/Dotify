a38d0ca688334a5dc8006e247c67a32ffe098ade
ThomasThat467 <thomas.tat@hotmail.com>
2020-04-08 03:23:30 -0700

    Added all content to layout and added some interaction with change user button

--- Dotify/app/src/main/java/com/thatt/dotify/MainActivity.kt
+++ Dotify/app/src/main/java/com/thatt/dotify/MainActivity.kt
@@ -2,7 +2,11 @@ package com.thatt.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.Spannable
+import android.text.SpannableStringBuilder
 import android.view.View
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +17,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun changeUser(view: View) {
-        user.text = "User is changed"
+        user.visibility = View.GONE
         button.text = "APPLY"
+        editText.visibility = View.VISIBLE
+        editText.setText(user.text as String)
     }
 }
