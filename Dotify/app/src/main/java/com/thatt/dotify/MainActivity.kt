e2a379e7d4dd2384f9cd4cad5beba9e8f41ce1b3
ThomasThat467 <thomas.tat@hotmail.com>
2020-04-08 21:37:24 -0700

    Added basic functionality (changing users, play/next/prev, play count)

--- Dotify/app/src/main/java/com/thatt/dotify/MainActivity.kt
+++ Dotify/app/src/main/java/com/thatt/dotify/MainActivity.kt
@@ -2,24 +2,45 @@ package com.thatt.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.Spannable
-import android.text.SpannableStringBuilder
 import android.view.View
-import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var plays = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        plays = Random.nextInt(1000, 1000000)
+        playCount.text = "$plays plays"
     }
 
     fun changeUser(view: View) {
-        user.visibility = View.GONE
-        button.text = "APPLY"
-        editText.visibility = View.VISIBLE
-        editText.setText(user.text as String)
+        if (userButton.text == "CHANGE USER") {
+            user.visibility = View.GONE
+            userButton.text = "APPLY"
+            editText.visibility = View.VISIBLE
+            editText.setText(user.text)
+        } else {
+            user.visibility = View.VISIBLE
+            userButton.text = "CHANGE USER"
+            editText.visibility = View.GONE
+            user.text = editText.text
+        }
+    }
+
+    fun incrementUp(view: View) {
+        plays++
+        playCount.text = "$plays plays"
+    }
+
+    fun nextClick(view: View) {
+        Toast.makeText(applicationContext,"Skipping to next track", Toast.LENGTH_SHORT).show()
+    }
+
+    fun prevClick(view: View) {
+        Toast.makeText(applicationContext,"Skipping to previous track", Toast.LENGTH_SHORT).show()
     }
 }
